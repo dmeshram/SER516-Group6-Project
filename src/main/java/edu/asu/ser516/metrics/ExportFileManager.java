60b7c4fb1b67c06151a8d533bbe4774f61f6827e
DeepanshuSaxena14 <dsaxen14@asu.edu>
2026-03-05 19:01:14 -0700

    Task #105: Updated ExportFileManager to copy on final destination

--- src/main/java/edu/asu/ser516/metrics/ExportFileManager.java
+++ src/main/java/edu/asu/ser516/metrics/ExportFileManager.java
@@ -3,6 +3,7 @@ package edu.asu.ser516.metrics;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class ExportFileManager {
 
@@ -17,5 +18,21 @@ public class ExportFileManager {
                 + "." + extension;
 
         return outputDir.resolve(fileName);
+    }
+
+    public static void atomicWrite(Path finalPath, byte[] content) throws IOException {
+        Path parent = finalPath.getParent();
+        if (parent != null && !Files.exists(parent)) {
+            Files.createDirectories(parent);
+        }
+        Path tmpPath = finalPath.resolveSibling(finalPath.getFileName() + ".tmp");
+        try {
+            Files.write(tmpPath, content);
+            Files.move(tmpPath, finalPath, StandardCopyOption.ATOMIC_MOVE,
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            Files.deleteIfExists(tmpPath);
+            throw e;
+        }
     }
 }
