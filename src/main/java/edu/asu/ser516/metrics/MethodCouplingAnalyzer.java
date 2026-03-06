65b8f101eaa2531b03db9e4c19b0dcea6b1556ad
DeepanshuSaxena14 <dsaxen14@asu.edu>
2026-03-06 01:53:49 -0700

    Task #60: Implemented getFanIn method

--- src/main/java/edu/asu/ser516/metrics/MethodCouplingAnalyzer.java
+++ src/main/java/edu/asu/ser516/metrics/MethodCouplingAnalyzer.java
@@ -84,4 +84,16 @@ public class MethodCouplingAnalyzer {
         return methodAdjacencyList.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().size()));
     }
+
+    public Map<String, Integer> getFanIn() {
+        Map<String, Integer> fanIn = new HashMap<>();
+        projectMethods.forEach(m -> fanIn.put(m, 0));
+        methodAdjacencyList.values().forEach(callees ->
+                callees.forEach(callee ->
+                        fanIn.put(callee, fanIn.getOrDefault(callee, 0) + 1)
+                )
+        );
+
+        return fanIn;
+    }
 }
